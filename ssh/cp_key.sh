a33ee14944ed1841e4c510ddc4bf68e014d21145
luojianxun <jxluo2087@gmail.com>
2017-04-17 12:20:31 +0800

    Update cp_key.sh

--- ssh/cp_key.sh
+++ ssh/cp_key.sh
@@ -7,5 +7,4 @@ fi
 
 ssh_server="$1"
 cd ~/.ssh && cat id_rsa.pub | ssh "$ssh_server" \
-    'mkdir -p ~/.ssh && chmod go-rwx ~/.ssh && cat >> .ssh/authorized_keys'
-#cd ~/.ssh && cat id_rsa.pub | ssh "$ssh_server" 'cat >> .ssh/authorized_keys'
+    'mkdir -p ~/.ssh && chmod go-rwx ~/.ssh && cat >> ~/.ssh/authorized_keys'
